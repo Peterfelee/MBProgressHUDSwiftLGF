31c86e64940f6c253192ca797385051cfdb27d89
peterlee <peterlee@wifi-lee.local>
2019-12-02 18:28:38 +0800

    添加一些自动显示的方法

--- MBProgressHUDSwiftLGF/Header/MBSwiftHelper.swift
+++ MBProgressHUDSwiftLGF/Header/MBSwiftHelper.swift
@@ -11,11 +11,10 @@ import UIKit
 public class MBSwiftHelper: NSObject {
     
     private var hud:MBProgressHudSwift?
-    
     public  static let share:MBSwiftHelper = MBSwiftHelper()
-    
     private var action:(()->())?
     
+    /**显示在view？ view为nil的时候显示在keywindow 需要手动隐藏 hidden*/
     public func show(InView view:UIView?,withAnimated animated:Bool = true)
     {
         hud?.removeFromSuperview()
@@ -51,38 +50,18 @@ public class MBSwiftHelper: NSObject {
 
         }
     }
+    
+    /**显示在view？ view为nil的时候显示在keywindow 需要手动隐藏 hidden*/
     public func show(InView view:UIView?)
     {
         show(InView: view, withAnimated: true)
     }
-    
+    /**显示在keywindow 需要手动隐藏 hidden*/
     public func show(){
         show(InView: nil)
     }
     
-    public func autoShow(InView view:UIView?,withAnimated animated:Bool = true,delayTime delay:Float = 1)
-    {
-        show(InView: view, withAnimated: animated)
-        hidden(delayTime: delay)
-    }
-    
-    public func autoShow(){
-        autoShow(InView: nil)
-    }
-    
-    public func hidden(withAnimated animated:Bool = true,delayTime delay:Float = 1)
-    {
-        if hud != nil {
-            hud!.hide(animated: animated, afterDelay: TimeInterval(delay))
-        }
-    }
-    
-    public func hidden(){
-        hidden(withAnimated: true, delayTime: 1)
-    }
-    
-    
-    //其他样式
+    /**显示在keywindow 单行的文字 需要手动隐藏 */
     public func showTitle(titleString title:String?){
         if title == nil || title!.isEmpty{
             show()
@@ -95,6 +74,7 @@ public class MBSwiftHelper: NSObject {
         }
     }
     
+    /**显示在keywindow 多行的文字 需要手动隐藏 */
     public func showMessage(Message msg:String?){
         if msg == nil || msg!.isEmpty{
             show()
@@ -107,7 +87,10 @@ public class MBSwiftHelper: NSObject {
         }
     }
     
-    ///必须含有按钮事件
+     
+    
+    
+     /**显示在keywindow 按钮样式可增加点击事件 需要手动隐藏 */
     public func showActionButton(buttonTitle title:String?,buttonImage image:UIImage?,buttonAction action:(()->())?){
         show()
         hud?.mode = .Text
@@ -117,6 +100,71 @@ public class MBSwiftHelper: NSObject {
         hud?.button.addTarget(self, action: #selector(buttonClick), for: .touchUpInside)
     }
     
+    
+    
+///hidden
+    
+      /**手动隐藏*/
+      public func hidden(withAnimated animated:Bool = true,delayTime delay:Float = 1)
+      {
+          if hud != nil {
+              hud!.hide(animated: animated, afterDelay: TimeInterval(delay))
+          }
+      }
+      
+      /**手动隐藏*/
+      public func hidden(){
+          hidden(withAnimated: true, delayTime: 1)
+      }
+ 
+////autoshow
+    /**显示在view？ view为nil的时候显示在keywindow 不需要手动隐藏*/
+    public func autoShow(InView view:UIView?,withAnimated animated:Bool = true,delayTime delay:Float = 1)
+    {
+        show(InView: view, withAnimated: animated)
+        hidden(delayTime: delay)
+    }
+    
+    /**显示在view？ view为nil的时候显示在keywindow 不需要手动隐藏*/
+    public func autoShow(){
+        autoShow(InView: nil)
+    }
+    
+    /**显示在keywindow 单行的文字 不需要手动隐藏 */
+    public func autoShowTitle(titleString title:String?){
+        if title == nil || title!.isEmpty{
+            autoShow()
+        }
+        else
+        {
+            autoShow()
+            hud?.mode = .Text
+            hud?.label.text = title
+        }
+    }
+    
+    /**显示在keywindow 多行的文字 不需要手动隐藏 */
+    public func autoShowMessage(Message msg:String?){
+        if msg == nil || msg!.isEmpty{
+            autoShow()
+        }
+        else
+        {
+            autoShow()
+            hud?.mode = .Text
+            hud?.detailLabel.text = msg
+        }
+    }
+    
+     /**显示在keywindow 按钮样式可增加点击事件 不需要手动隐藏 */
+    public func autoShowActionButton(buttonTitle title:String?,buttonImage image:UIImage?,buttonAction action:(()->())?){
+        autoShow()
+        hud?.mode = .Text
+        hud?.button.setTitle(title, for: .normal)
+        hud?.button.setImage(image, for: .normal)
+        self.action = action
+        hud?.button.addTarget(self, action: #selector(buttonClick), for: .touchUpInside)
+    }
     
     
 }
--- MBProgressHUDSwiftLGF/ViewController.swift
+++ MBProgressHUDSwiftLGF/ViewController.swift
@@ -36,7 +36,7 @@ extension ViewController{
     @objc private func testClick(btn:UIButton){
         switch btn.tag {
         case 1://展示一个单行的文字，过长的话自动省略显示
-            MBSwiftHelper.share.showTitle(titleString: btn.currentTitle)
+            MBSwiftHelper.share.autoShowTitle(titleString: btn.currentTitle)
         case 2://展示一个多行的文字，过长的话自动换行
             MBSwiftHelper.share.showMessage(Message: btn.currentTitle)
         case 3://展示透明背景的菊花转 可以自动移除
